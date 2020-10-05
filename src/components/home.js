7dda9fe57bb1c6e7cb4fa349ec44acde76042a5d
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-04 22:47:34 -0400

    page one is fully responsive

--- src/components/home.js
+++ src/components/home.js
@@ -3,8 +3,13 @@ import {useCallback} from 'react';
 import { useHistory } from 'react-router-dom';
 import { Frame, Stack } from "framer";
 import { motion, AnimatePresence } from "framer-motion";
+import Responsive from 'react-responsive';
 import '../App.scss';
 
+
+const Desktop = props => <Responsive {...props} minWidth={992} />;
+const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
+const Mobile = props => <Responsive {...props} maxWidth={767} />;
 
 export default function Home(props) {
   const history = useHistory();
@@ -16,33 +21,95 @@ export default function Home(props) {
         width={"100%"}
         height={"100%"}
         >
-        <Frame
-          background={'black'}
-          center
-          animate={{
-            width: ['0%', '10%', '30%', '50%', '130%'],
-            height: ['0%', '10%', '50%', '50%', '150%'],
-            borderRadius: ["5%", "5%", "5%", "5%", "5%"]
-          }}
-          transition={{
-            delay:1.5,
-            duration: 2,
-            ease: "easeInOut",
-            times: [0, 0.2, 0.5, 0.8, 1],
-          }}
-          style={{fontSize:50, color:'white'}}
-          onClick={handleOnClick}
-          >
-            <Frame
-              center
-              background={'black'}
-              initial={{ opacity: 0}}
-              animate={{ opacity: 1}}
-              transition={{ duration: 0.5, delay:3.5 }}
-              >
-                Hello!
+        <Desktop>
+          <Frame
+            background={'black'}
+            center
+            animate={{
+              width: ['0%', '10%', '30%', '50%', '100%'],
+              height: ['0%', '10%', '50%', '50%', '100%'],
+              borderRadius: ["5%", "5%", "5%", "5%", "0%"]
+            }}
+            transition={{
+              delay:1.5,
+              duration: 2,
+              ease: "easeInOut",
+              times: [0, 0.2, 0.5, 0.8, 1],
+            }}
+            style={{fontSize:50, color:'white'}}
+            onClick={handleOnClick}
+            >
+              <Frame
+                center
+                background={'black'}
+                initial={{ opacity: 0}}
+                animate={{ opacity: 1}}
+                transition={{ duration: 0.5, delay:3.5 }}
+                >
+                  Hello!
               </Frame>
-        </Frame>
+            </Frame>
+        </Desktop>
+
+        <Mobile>
+          <Frame
+            background={'black'}
+            center
+            animate={{
+              width: ['0%', '20%', '50%', '50%', '100%'],
+              height: ['0%', '5%', '20%', '50%', '100%'],
+              borderRadius: ["5%", "5%", "5%", "5%", "0%"]
+            }}
+            transition={{
+              delay:1.5,
+              duration: 2,
+              ease: "easeInOut",
+              times: [0, 0.2, 0.5, 0.8, 1],
+            }}
+            style={{ fontSize:70, color:'white' }}
+            onClick={handleOnClick}
+            >
+              <Frame
+                center
+                background={'black'}
+                initial={{ opacity: 0}}
+                animate={{ opacity: 1}}
+                transition={{ duration: 0.5, delay:3.5 }}
+                >
+                 Hello!
+              </Frame>
+            </Frame>
+        </Mobile>
+
+        <Tablet>
+          <Frame
+            background={'black'}
+            center
+            animate={{
+              width: ['0%', '20%', '50%', '70%', '100%'],
+              height: ['0%', '5%', '50%', '50%', '100%'],
+              borderRadius: ["5%", "5%", "5%", "5%", "0%"]
+            }}
+            transition={{
+              delay:1.5,
+              duration: 2,
+              ease: "easeInOut",
+              times: [0, 0.2, 0.5, 0.8, 1],
+            }}
+            style={{ fontSize:90, color:'white' }}
+            onClick={handleOnClick}
+            >
+              <Frame
+                center
+                background={'black'}
+                initial={{ opacity: 0}}
+                animate={{ opacity: 1}}
+                transition={{ duration: 0.5, delay:3.5 }}
+                >
+                 Hello!
+              </Frame>
+            </Frame>
+        </Tablet>
       </Frame>
     </AnimatePresence>
   );
