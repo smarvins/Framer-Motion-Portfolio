2a9552afb18ab9d1ff8dd83d6dd0f5a8e665ae8b
Shavolski-dev <stevewachiradev@gmail.com>
2020-10-09 18:28:03 -0400

    added skills section

--- src/components/services.js
+++ src/components/services.js
@@ -44,7 +44,7 @@ export default function Services(props) {
           initial="hidden"
           animate="show"
           >
-          <Frame variants={item} background="black" width={1200} height={70} radius="5%" style={{color:'white', fontSize:70}}><span>These are the <span style={{color:'rgb(156, 26, 255)'}}>cool</span> stuff I can do for <span style={{color:'rgb(156, 26, 255)', fontStyle: 'italic'}}>you</span>.</span></Frame>
+          <Frame variants={item} background="black" width={1200} height={70} radius="5%" style={{color:'white', fontSize:70}}><span>These are the <span style={{color:'#7F00FF'}}>cool</span> stuff I can do for <span style={{color:'#7F00FF', fontStyle: 'italic'}}>you</span>.</span></Frame>
         </Stack>
 
         <Frame top={250} left={70} width={1150} height={500} background="black">
@@ -55,10 +55,10 @@ export default function Services(props) {
             width={1100} height={300}
             padding={20}
             >
-              <Frame radius="3%" background="linear-gradient(180deg, #d309e1 0%, rgb(156, 26, 255) 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Responsive Design</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>Go on and resize the window, see for yourself.</Frame></Frame>
-              <Frame radius="3%" background="linear-gradient(180deg, #d309e1 0%, rgb(156, 26, 255) 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Web Development</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>Having experience on creating both front and backend applications, I'm confident in my skills to get the job done.</Frame></Frame>
-              <Frame radius="3%" background="linear-gradient(180deg, #d309e1 0%, rgb(156, 26, 255) 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Debugging</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>After spending many sleepless nights solving bugs and errors, I've got hawk eyes to go solve them and documenting for future reference.</Frame></Frame>
-              <Frame radius="3%" background="linear-gradient(180deg, #d309e1 0%, rgb(156, 26, 255) 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Deployment</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>Having deployed from singular to hybrid apps. I have a knack for delivering applications to different hosting sites.</Frame></Frame>
+              <Frame borderRadius={10} background="linear-gradient(180deg, #E100FF 0%, #7F00FF 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Responsive Design</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>Go on and resize the window, see for yourself.</Frame></Frame>
+              <Frame borderRadius={10} background="linear-gradient(180deg, #E100FF 0%, #7F00FF 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Web Development</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>Having experience on creating both front and backend applications, I'm confident in my skills to get the job done.</Frame></Frame>
+              <Frame borderRadius={10} background="linear-gradient(180deg, #E100FF 0%, #7F00FF 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Debugging</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>After spending many sleepless nights solving bugs and errors, I've got hawk eyes to go solve them and documenting for future reference.</Frame></Frame>
+              <Frame borderRadius={10} background="linear-gradient(180deg, #E100FF 0%, #7F00FF 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Deployment</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>Having deployed from singular to hybrid apps. I have a knack for delivering applications to different hosting sites.</Frame></Frame>
           </Stack>
         </Frame>
       </Frame>
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,37 +1,22 @@
 import React from 'react';
-import {useCallback} from 'react';
 import { useHistory } from 'react-router-dom';
-import { Frame, Stack, Scroll } from "framer";
+import { Frame, Stack, Scroll, useCycle } from "framer";
 import { motion, AnimatePresence } from "framer-motion";
 import '../App.scss';
 
-
-const container = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.3
-    }
-  }
-}
-
-const item = {
-  hidden: { opacity: 0 },
-  show: { opacity: 1 }
-}
-
-
-export default function Skills(props) {
-  const history = useHistory();
-  const handleOnClick = useCallback(() => history.push('/'), [history]);
+export default function Skills() {
+  const [animate, cycle] = useCycle(
+      { width: 50, height: 60, background:"white", color:"transparent" },
+      { width: 800, height: 60, background:"orange", color:"white" }
+  )
+  const [animate1, cycle1] = useCycle(
+      { width: 50, height: 60, background:"white", color:"transparent" },
+      { width: 500, height: 60, background:"orange", color:"white" }
+  )
   return (
     <AnimatePresence>
       <Frame
         background={'black'}
-        variants={container}
-        initial="hidden"
-        animate="show"
         >
         <Stack
           style={{ fontWeight: 'bold'}}
@@ -40,27 +25,56 @@ export default function Skills(props) {
           paddingBottom={60}
           paddingLeft={70}
           alignment="start"
-          variants={container}
-          initial="hidden"
-          animate="show"
           >
-          <Frame variants={item} background="black" width={1200} height={70} radius="5%" style={{color:'white', fontSize:70}}><span>The <span style={{color:'#fe8c00'}}>super powers</span> I got.</span></Frame>
+          <Frame background="black" width={1200} height={70} radius="5%" style={{color:'white', fontSize:70}}><span>The <span style={{color:'#ffc500'}}>super powers</span> I got.</span></Frame>
         </Stack>
 
-        <Frame top={250} left={70} width={1150} height={500} background="black">
-          <Stack
-            center
-            distribution="space-evenly"
-            direction="horizontal"
-            width={1100} height={300}
-            padding={20}
-            >
-              <Frame radius="3%" background="linear-gradient(180deg, #d309e1 0%, rgb(156, 26, 255) 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Responsive Design</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>Go on and resize the window, see for yourself.</Frame></Frame>
-              <Frame radius="3%" background="linear-gradient(180deg, #d309e1 0%, rgb(156, 26, 255) 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Web Development</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>Having experience on creating both front and backend applications, I'm confident in my skills to get the job done.</Frame></Frame>
-              <Frame radius="3%" background="linear-gradient(180deg, #d309e1 0%, rgb(156, 26, 255) 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Debugging</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>After spending many sleepless nights solving bugs and errors, I've got hawk eyes to go solve them and documenting for future reference.</Frame></Frame>
-              <Frame radius="3%" background="linear-gradient(180deg, #d309e1 0%, rgb(156, 26, 255) 100%)" width={250} height={450} style={{color:'white', paddingLeft:'10px', fontSize:30}}><span>Deployment</span><Frame center background="transparent" style={{color:'white', fontSize:20}}>Having deployed from singular to hybrid apps. I have a knack for delivering applications to different hosting sites.</Frame></Frame>
-          </Stack>
+        <Frame background="black" width={550} left={73} height={100} top={150} style={{color:'white', fontSize:25, fontWeight:'100', wordSpacing: '3px', lineHeight:'45px'}}>
+          Here's a list of <span style={{color:'#ffc500'}}>skills</span> that I learned over the past years as a developer.
         </Frame>
+
+        <Frame top={300} left={70} width={1150} height={500} background="black" borderRadius={30}>
+
+          <Frame width={250} height={60} borderRadius={10} backgroundColor="orange" >
+              [ Web Frameworks ]
+          </Frame>
+          <Frame left={230} backgroundColor="#fff" borderRadius={10} animate={animate} onTap={() => cycle()} style={{ fontSize:20}}>
+              React / Django
+          </Frame>
+
+          <Frame top={100} width={250} height={60} borderRadius={10} backgroundColor="orange" >
+              [ UI Frameworks ]
+          </Frame>
+          <Frame top={100} left={230} backgroundColor="#fff" borderRadius={10} animate={animate} onTap={() => cycle()} style={{ fontSize:20}}>
+              Bootstrap / Material UI / Bulma / Framer Motion
+          </Frame>
+
+          <Frame top={200} width={250} height={60} borderRadius={10} backgroundColor="orange" >
+              [ Markup & Style Sheets ]
+          </Frame>
+          <Frame top={200} left={230} backgroundColor="#fff" borderRadius={10} animate={animate} onTap={() => cycle()} style={{ fontSize:20}}>
+              HTML / JSX / CSS / SCSS / JSS
+          </Frame>
+          <Frame top={300} width={250} height={60} borderRadius={10} backgroundColor="orange" >
+              [ Languages ]
+          </Frame>
+          <Frame top={300} left={230} backgroundColor="#fff" borderRadius={10} animate={animate} onTap={() => cycle()} style={{ fontSize:20}}>
+              Python / JavaScript
+          </Frame>
+          <Frame top={400} width={250} height={60} borderRadius={10} backgroundColor="orange" >
+              [ Hosting Sites ]
+          </Frame>
+          <Frame top={400} left={230} backgroundColor="#fff" borderRadius={10} animate={animate} onTap={() => cycle()} style={{ fontSize:20}}>
+              GitHub / Netlify / Heroku
+          </Frame>
+          <Frame top={500} width={250} height={60} borderRadius={10} backgroundColor="orange" >
+              [ YouTube ]
+          </Frame>
+          <Frame top={500} left={230} backgroundColor="#fff" borderRadius={10} animate={animate} onTap={() => cycle()} style={{ fontSize:20}}>
+              Channel Management / Video Editing / SEO / Thumbnail Creation
+          </Frame>
+        </Frame>
+
       </Frame>
     </AnimatePresence>
   );
